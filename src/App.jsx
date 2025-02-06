c84e2520d1ed098c1479c1aa400102385a8b945a
dundun003 <liangshaojun@papegames.net>
2025-02-06 11:14:22 +0800

    feat: 初始化Demo组件和list组件

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,37 @@
-import './App.css';
+import React, { useState } from 'react';
+import { Radio } from 'antd';  
+// import DataList from './components/DataList';
+// import CanvasDemo from './components/CanvasDemo';
+import './App.css'; 
 
-const App = () => {
+function App() {
+  const [radio, setRadio] = useState(1);
+
+  const handleRadioChange = (e) => {
+    setRadio(e.target.value);
+  };
+
   return (
-    <div className="content">
-      <h1>Rsbuild with React</h1>
-      <p>Start building amazing things with Rsbuild.</p>
+    <div id="app">
+      <div className="changeRadio">
+        <Radio.Group 
+          value={radio} 
+          onChange={handleRadioChange}
+          style={{ 
+            margin: '0 auto', 
+            display: 'flex', 
+            justifyContent: 'center' 
+          }}
+        >
+          <Radio value={1}>表格计算示例</Radio>
+          <Radio value={2}>canvas绘制示例</Radio>
+        </Radio.Group>
+      </div>
+
+      {/* {radio === 1 && <DataList />}
+      {radio === 2 && <CanvasDemo />} */}
     </div>
   );
-};
+}
 
 export default App;
